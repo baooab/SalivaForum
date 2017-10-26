c1a362a64de6aa1920fc52c570c3e681eaf84c54
zhangbao <3183442656@qq.com>
2017-10-26 16:44:33 +0800

    fix bugs

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', '3183442656@qq.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'example@example.com'),
         'name' => env('MAIL_FROM_NAME', '乱炖社区'),
     ],
 
